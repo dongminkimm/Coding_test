df5cf9033b60ec76e02eeddafa8ce1467b2bb36a
dongminkimm <kdm753@gmail.com>
2024-04-01 15:51:36 +0900

    0401

--- For_coding_test/10.Graph/10-8.py
+++ For_coding_test/10.Graph/10-8.py
@@ -1,13 +1,35 @@
-n, m = map(int, input().split())
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
+n, m = map(int, input().split())
+parent = [0] * (n+1)
 result = 0
-graph = [[] for _ in range(n+1)]
+edges = []
+
+for i in range(1, n+1):
+    parent[i] = i
 
 for _ in range(m):
     a, b, cost = map(int, input().split())
-    graph[a].append((cost, b))
+    edges.append((cost, a, b))
 
-for i in graph:
-    i.sort()
+edges.sort()
+last = 0
 
-print(graph)
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - cost)
---
+++ For_coding_test/10.Graph/10-9.py
@@ -0,0 +1,19 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n = int(input())
+li = []
+for i in range(n):
+    li.append(list(map(int, input().split())))
+
+print(li)
